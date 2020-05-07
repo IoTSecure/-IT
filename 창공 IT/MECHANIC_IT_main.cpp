8d255f627a1eb956f447caef01dba2efb76a63ee
IoTSecure <JChanho99@gmail.com>
2020-05-07 15:45:05 +0900

    추가 파일 생성.

---
+++ 창공 IT/MECHANIC_IT_main.cpp
@@ -0,0 +1,19 @@
+//
+//  MECHANIC_IT_main.cpp
+//  창공 IT
+//
+//  Created by 전찬호 on 2020/05/07.
+//  Copyright © 2020 전찬호. All rights reserved.
+//
+
+#include "MECHANIC_IT_main.hpp"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!";
+    
+    return 0;
+}
---
+++ 창공 IT/MECHANIC_IT_main.hpp
@@ -0,0 +1,14 @@
+//
+//  MECHANIC_IT_main.hpp
+//  창공 IT
+//
+//  Created by 전찬호 on 2020/05/07.
+//  Copyright © 2020 전찬호. All rights reserved.
+//
+
+#ifndef MECHANIC_IT_main_hpp
+#define MECHANIC_IT_main_hpp
+
+#include <stdio.h>
+
+#endif /* MECHANIC_IT_main_hpp */
