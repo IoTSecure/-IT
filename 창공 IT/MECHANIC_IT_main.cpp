8a31e75c668305ecbe89295c5ace41e8a0b4b1e9
전찬호 <JChanho99@gmail.com>
2020-05-21 15:00:36 +0900

    Merged

---
+++ 창공 IT/MECHANIC_IT_main.cpp
@@ -0,0 +1,19 @@
+//
+//  MECHANIC_IT_main.cpp
+//  창공 IT
+//
+//  Created by 전찬호 on 2020/05/07.
+//  Copyright © 2020 전찬호. All rights reserved.
+//
+
+#include "MECHANIC_IT_main.hpp"
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, World!\n";
+    cout << "Hard to know people's mind..";
+    return 0;
+}
